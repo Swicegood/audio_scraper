ecdc32b3c76263db9636a4045cd6690a711e0b19
Swicegood <jguru@gmail.com>
2024-04-20 20:34:16 -0400

    change ftp to scp

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import configparser
 import datetime
 import asyncio
 import audio_scraper
-from ftplib import FTP
+import subprocess
+import os
 
 BADFILESFILE = 'bad_files.txt'
 
@@ -17,29 +18,14 @@ def remove_bad_files(files):
     return files
 
 def upload_files_to_server(files, remote_path):
-    # Read configuration file for credentials
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    hostname = config['FTP']['hostname']
-    username = config['FTP']['username']
-    password = config['FTP']['password']
-
-    # Connect to the server using FTP
-    ftp = FTP(hostname)
-    ftp.login(user=username, passwd=password)
 
     # Write the file list to a file
     with open('all_files.txt', 'w') as f:
         f.write('#' + str(datetime.datetime.now()) + '\n') 
         for file in files:
             f.write(file + '\n')
-
-    # Upload the file to the remote server
-    with open('all_files.txt', 'rb') as f:
-        ftp.storbinary('STOR ' + remote_path, f)
-
-    # Close the FTP connection
-    ftp.quit()
+    # Step 2: Copy file from host to remote server
+    subprocess.run(["scp", "-i", "id_rsa_atourcity",  "all_files.txt", "atourcity@newgoloka.com:public_html/bkgoswami.com/wp/wp-content/uploads/"])
 
 async def main():
     all_files = await audio_scraper.get_all_files()
