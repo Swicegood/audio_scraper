fdf5dc819d979619767c445811eaac815f552ed0
Jagadguru <jguru108@gmail.com>
2024-12-07 11:28:36 -0500

    revert

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import configparser
 import datetime
 import asyncio
 import audio_scraper
-import yaml
 import subprocess
 import os
 
@@ -16,17 +15,6 @@ def remove_bad_files(files):
 
     # Remove the bad files from the list
     files = [file for file in files if file not in bad_files]
-    return files
-
-def rename_problem_files(files):
-    # Read in dictionary of problem files from file renames.yaml
-    with open('renames.yaml', 'r') as f:
-        renames = yaml.safe_load(f)
-    # Rename the files
-    for i, file in enumerate(files):
-        if file in renames:
-            files[i] = renames[file]
-            print(f"Renamed {file} to {renames[file]}")
     return files
 
 def upload_files_to_server(files, remote_path):
@@ -46,8 +34,6 @@ async def main():
     all_files = remove_bad_files(all_files)
     print("Total files after removing bad files:", len(all_files))
     print("Bad files removed ", total_files - len(all_files))
-    all_files = rename_problem_files(all_files)
-    print("Renamed problem files")
     print("Uploading files to server...")
     upload_files_to_server(all_files, "/wp/wp-content/uploads/all_files.txt")
 
